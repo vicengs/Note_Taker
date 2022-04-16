1c4e063dbcb6dc460a963703e447b3ef07eff1bc
Vicente Garcia <vicen1@hotmail.com>
2022-04-15 22:04:28 -0500

    Create server and listener

---
+++ server.js
@@ -0,0 +1,22 @@
+/* ------------------------- */
+/* Project  : Note Taker     */
+/* File     : server.js      */
+/* Author   : Vicente Garcia */
+/* Date     : 04/15/2022     */
+/* Modified : 04/15/2022     */
+/* ------------------------- */
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+// parse incoming string or array data
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
