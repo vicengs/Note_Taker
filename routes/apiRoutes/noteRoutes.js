ce0ac8c39a6da2974b05c1bcbcd4b0d6c4349991
Vicente Garcia <vicen1@hotmail.com>
2022-04-17 00:03:59 -0500

    Add delete function

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -6,7 +6,7 @@
 /* Modified : 04/16/2022      */
 /* -------------------------- */
 const router = require("express").Router();
-const { filterByQuery, findById, createNewNote, validateNote } = require("../../lib/notes");
+const { filterByQuery, findById, createNewNote, deleteNote, validateNote } = require("../../lib/notes");
 const { notes } = require("../../data/db");
 router.get("/notes", (req, res) => {
     let results = notes;
@@ -25,7 +25,14 @@ router.get("/notes/:id", (req, res) => {
 });
 router.post("/notes", (req, res) => {
     // set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
+    //req.body.id = notes.length.toString();
+    let maxId = 0;
+    for (let i=0; i<notes.length; i++){
+        if (parseInt(notes[i].id) >= maxId){
+            maxId = parseInt(notes[i].id) + 1;
+        };
+    };
+    req.body.id = maxId.toString();
     // req.body is where our incoming content will be
     // add animal to json file and animals array in this function
     // if any data in req.body is incorrect, send 400 error back
@@ -35,5 +42,9 @@ router.post("/notes", (req, res) => {
         const note = createNewNote(req.body, notes);
         res.json(note);
     };
+});
+router.delete("/notes", (req, res) => {
+    const suprNote = deleteNote(req.body.id, notes);
+    res.json(suprNote);
 });
 module.exports = router;
